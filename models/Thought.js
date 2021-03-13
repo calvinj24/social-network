80f02d8a5d13d7bcf7eeb7f7713bd267ee7aac63
Calvin Johnson <calvinjohnson@Calvins-MacBook-Pro-7.local>
2021-03-12 22:14:04 -0600

    add reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,36 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -18,7 +48,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true
     },
-    reactions: []
+    reactions: [ReactionSchema]
   },
   {
     toJSON: {
@@ -28,6 +58,10 @@ const ThoughtSchema = new Schema(
     id: false
   }
 )
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
